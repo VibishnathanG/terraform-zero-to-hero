85312c6ccffef8b9aa5b07ba0e51a2cf67534fe2
vibishnathanWSL <vibishnatha@gmail.com>
2024-02-12 15:48:02 +0530

    main file commit

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,32 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "ap-south-1" # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "THis is the instance Type"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "This is the ami id"
+}
+
+
+resource "aws_instance" "ec2_01" {
+  #ami           = "ami-0d92749d46e71c34c" # Specify an appropriate AMI ID
+  ami = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = "subnet-0f6ac7881fc90d49b"
+  tags = {
+    name = "EC2-01"
+  }
+
+}
+
+output "public_ip" {
+  value       = aws_instance.ec2_01.private_ip
+  description = "Printing Public IP"
+
 }
